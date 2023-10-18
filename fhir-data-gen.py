dcf174a9c5cfb7ac932f37bb7c0537ae15df033b
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2023-10-18 20:10:56 +0200

    add user feedbac

--- fhir-data-gen.py
+++ fhir-data-gen.py
@@ -5,11 +5,48 @@ from jinja2 import Environment, BaseLoader
 import json
 import os
 import random
+import importlib.resources
+#import sys
 
 # Lookup function for the Jinja2 template
 
 def random_up_to(x):
     return random.randint(0, x)
+
+
+def read_config_file(filename="config.json"):
+    """Read the configuration file and return its content as a dictionary."""
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+def show_usage_and_wait():
+    """Display the usage of the script and wait for user input."""
+    usage = """
+    FHIR Data Generator
+    -------------------
+    A Python script that generates synthetic data for the FHIR (Fast Healthcare Interoperability Resources) standard.
+    The script reads data from a CSV file and generates FHIR resource instances in FSH (FHIR Shorthand) format.
+
+    Usage:
+    -----
+    -i, --input     : The path to the input CSV file containing the data to be converted. This parameter is required.
+    -t, --template  : The path to the Jinja2 template file to use for generating the FSH output. This parameter is required.
+    -o, --output    : The path to the output directory where the generated FSH files will be saved (default: current directory).
+    -n, --name      : The name to use for the output FSH file (default: 'Data').
+
+    Alternatively, when executed without arguments, the argument values will be taken from the file config.json
+
+    Example:
+    -------
+    script_name -i input.csv -t template.fsh.jinja -o output -n Patient
+
+    Press Enter to exit...
+    """
+    print(usage)
+    input()
 
 
 def lookup(value, table_name, tables):
@@ -38,32 +75,49 @@ def load_all_tables_from_folder(folder_path):
 
     
 if __name__ == '__main__':
+    config = read_config_file()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output', '-o', type=pathlib.Path, default='.', help='output directory')
-    parser.add_argument('--data', '-d', type=pathlib.Path, required=True, help='data folder path')
-    parser.add_argument('--template', '-t', type=pathlib.Path, required=True, help='FSH template file path')
-    parser.add_argument('--name', '-n', type=str, default='Data', help='name of the output file')
+
     args = parser.parse_args()
+    if not hasattr(args, 'input') or not args.input or not hasattr(args, 'template') or not args.template:
+        show_usage_and_wait()
+        exit(0)
+    try:
+        parser.add_argument('--output', '-o', type=pathlib.Path, default='.', help='output directory')
+        parser.add_argument('--data', '-d', type=pathlib.Path, required=True, help='data folder path')
+        parser.add_argument('--template', '-t', type=pathlib.Path, required=True, help='FSH template file path')
+        parser.add_argument('--name', '-n', type=str, default='Data', help='name of the output file')
 
-    fsh_template_text = args.template.read_text()
+        fsh_template_text = args.template.read_text()
 
-    # Set up Jinja2 environment and add the lookup function
-    env = Environment(loader=BaseLoader())
-    env.globals['lookup'] = lambda value, table_name: lookup(value, table_name, all_tables)
-    env.globals['random'] = random_up_to
-    template = env.from_string(fsh_template_text)
+        # Set up Jinja2 environment and add the lookup function
+        env = Environment(loader=BaseLoader())
+        env.globals['lookup'] = lambda value, table_name: lookup(value, table_name, all_tables)
+        env.globals['random'] = random_up_to
+        template = env.from_string(fsh_template_text)
 
-    # Load all tables from the specified folder
-    all_tables = load_all_tables_from_folder(args.data)
-#    print(all_tables)
+        # Load all tables from the specified folder
+        all_tables = load_all_tables_from_folder(args.data)
+    #    print(all_tables)
 
 
-    # create output dir if doesn't exist
-    pathlib.Path.mkdir(args.output, parents=True, exist_ok=True)
+        # create output dir if doesn't exist
+        pathlib.Path.mkdir(args.output, parents=True, exist_ok=True)
 
-    # Render the template with the dataframe and all loaded tables
-    output_str = template.render(data=all_tables)
+        # Render the template with the dataframe and all loaded tables
+        output_str = template.render(data=all_tables)
 
-    print(f'Generating {args.name}...')
-    with open(args.output / f'{args.name}.fsh', 'wt') as output_file:
-        output_file.write(output_str)
+        print(f'Generating {args.name}...')
+        with open(args.output / f'{args.name}.fsh', 'wt') as output_file:
+            output_file.write(output_str)
+
+            print("\nSummary:")
+            print("-------")
+            print(f"Input file processed: {args.input}")
+            print(f"Template used: {args.template}")
+            print(f"Output directory: {args.output}")
+            print(f"Name of the output file: {args.name}.fsh")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        input("Press Enter to exit...")        
